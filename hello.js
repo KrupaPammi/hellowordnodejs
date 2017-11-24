e11c94d6dff3888b114ee2647e69d08ce0872ebe
krupa <k.pammi@comicrelief.com>
2017-11-24 13:42:46 +0000

    change string

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-	  res.send('Hello Krupa!')
+	  res.send('Hello World!')
 })
 
 module.exports.handler = serverless(app);
